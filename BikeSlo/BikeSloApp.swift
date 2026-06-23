df51dd166be081471c4a971701f0abf2ef49a01c
Matija Lukanc <lukancmatija@outlook.com>
2026-06-23 21:37:34 +0200

    Initial Commit

---
+++ BikeSlo/BikeSloApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeSloApp.swift
+//  BikeSlo
+//
+//  Created by Matija Lukanc on 23. 6. 2026.
+//
+
+import SwiftUI
+
+@main
+struct BikeSloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeSlo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeSlo
+//
+//  Created by Matija Lukanc on 23. 6. 2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
